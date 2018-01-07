e37ac5192095e41300c7820215e9a30fb331f10d
Vu Tran <vutran.270797@gmail.com>
2018-01-08 01:25:08 +0700

    implement basic homepage

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-$('.carousel').carousel({
-  interval: false
+$('#carousel-example-generic').carousel({
+  interval: 3000
+});
+
+$('.carosel-control-right').click(function() {
+  $(this).blur();
+  $(this).parent().find('.carosel-item').first().insertAfter($(this).parent().find('.carosel-item').last());
+});
+$('.carosel-control-left').click(function() {
+  $(this).blur();
+  $(this).parent().find('.carosel-item').last().insertBefore($(this).parent().find('.carosel-item').first());
 });
