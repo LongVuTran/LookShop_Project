045f1bb0947e5f41a9868a96462a5d1933a66ea6
Vu Tran <vutran.270797@gmail.com>
2018-01-10 01:10:27 +0700

    adjusted hover

--- js/script.js
+++ js/script.js
@@ -1,12 +1,39 @@
-$('#carousel-example-generic').carousel({
-  interval: 3000
-});
+// $('#carousel-example-generic').carousel({
+//   interval: 3000
+// });
 
-$('.carosel-control-right').click(function() {
-  $(this).blur();
-  $(this).parent().find('.carosel-item').first().insertAfter($(this).parent().find('.carosel-item').last());
-});
-$('.carosel-control-left').click(function() {
-  $(this).blur();
-  $(this).parent().find('.carosel-item').last().insertBefore($(this).parent().find('.carosel-item').first());
+// $('.carosel-control-right').click(function() {
+//   $(this).blur();
+//   $(this).parent().find('.carosel-item').first().insertAfter($(this).parent().find('.carosel-item').last());
+// });
+// $('.carosel-control-left').click(function() {
+//   $(this).blur();
+//   $(this).parent().find('.carosel-item').last().insertBefore($(this).parent().find('.carosel-item').first());
+// });
+
+$('.center').slick({
+  centerMode: true,
+  centerPadding: '6px',
+  slidesToShow: 5,
+  slideToScroll: 5,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 4
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
 });
