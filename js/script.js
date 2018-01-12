a2af9b3fba55735bff75457485c1e9255f55908f
Vu Tran <vutran.270797@gmail.com>
2018-01-13 01:03:27 +0700

    fixed slide homepage

--- js/script.js
+++ js/script.js
@@ -1,39 +1,36 @@
-// $('#carousel-example-generic').carousel({
-//   interval: 3000
-// });
+$(document).ready(function() {
+	$('.center').slick({
+		centerMode: true,
+		infinite: true,
+		centerPadding: '90px',
+		slidesToShow: 4,
+		variableWidth: false,
+		responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '40px',
+				slidesToShow: 3
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '40px',
+				slidesToShow: 1
+			}
+		}
+		]
+	});
 
-// $('.carosel-control-right').click(function() {
-//   $(this).blur();
-//   $(this).parent().find('.carosel-item').first().insertAfter($(this).parent().find('.carosel-item').last());
-// });
-// $('.carosel-control-left').click(function() {
-//   $(this).blur();
-//   $(this).parent().find('.carosel-item').last().insertBefore($(this).parent().find('.carosel-item').first());
-// });
-
-$('.center').slick({
-  centerMode: true,
-  centerPadding: '6px',
-  slidesToShow: 5,
-  // slideToScroll: 4,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 4
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
+	$('.center').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+		console.log('beforeChange', currentSlide, nextSlide);
+	});
+	$('.center').on('afterChange', function(event, slick, currentSlide){
+		console.log('afterChange', currentSlide);
+	});
 });
